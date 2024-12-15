3db7b469831059a4e9311cad901a9e920b18b457
vagner <vagnerseibert@gmail.com>
2024-12-15 14:57:23 -0300

    Added data analysis for entire db. Adjusted db_wrapper to load an yaml for auth and increased the txt character size. Small polishments also included.

--- src/db_wrapper.py
+++ src/db_wrapper.py
@@ -1,5 +1,9 @@
-import psycopg2, json
-conn_str = "dbname=emb user=henry"
+import psycopg2, json, yaml
+
+auth = yaml.safe_load(open('../auth/db.yaml'))
+dbname, user = auth['dbname'], auth['user']
+conn_str = f"dbname={dbname} user={user}"
+table_name = 'bcorpus'
 
 def execute_query(query, commit=False, fetch=False):
     conn = psycopg2.connect(conn_str)
@@ -20,15 +24,19 @@ def execute_query(query, commit=False, fetch=False):
     return r
 
 def insert(doc, page, emb, txt):
-    query = f"INSERT INTO bcorpus (doc, page, embedding, txt) VALUES ({doc}, {page}, '{emb}', '{txt}');"
+    query = f"INSERT INTO {table_name} (doc, page, embedding, txt) VALUES ({doc}, {page}, '{emb}', '{txt}');"
+    execute_query(query, True)
+
+def delete_table():
+    query = f'DROP TABLE {table_name};'
     execute_query(query, True)
 
 def semantic_search(emb, lim=10):
-    query = f"SELECT * FROM bcorpus ORDER BY embedding <-> '{emb}' LIMIT {lim};"
+    query = f"SELECT * FROM {table_name} ORDER BY embedding <-> '{emb}' LIMIT {lim};"
     return execute_query(query, fetch=True)
 
 def retrieve(doc, page):
-    query = f"SELECT * FROM bcorpus WHERE doc={doc} AND page={page-1}"
+    query = f"SELECT * FROM {table_name} WHERE doc={doc} AND page={page-1}"
     return execute_query(query, fetch=True)[0]
 
 def setup_pg():
@@ -38,19 +46,20 @@ def setup_pg():
     cur = conn.cursor()
 
     #cur.execute("CREATE EXTENSION vector;") # Execute this to enable embeddings, if this wasn't already
-    cur.execute("CREATE TABLE bcorpus (id bigserial PRIMARY KEY, doc INT, page INT, txt VARCHAR(10000), embedding vector(768));")
+    cur.execute(f"CREATE TABLE {table_name} (id bigserial PRIMARY KEY, doc INT, page INT, txt VARCHAR(20000), embedding vector(768));")
 
     js = json.loads(open(f'{parsed_folder}corpus.json').read())
 
     for it in js:
         txt, emb, doc, page = it['txt'], it['emb'], it['doc'], it['page']
         txt = txt.replace("'", '')
-        query = f"INSERT INTO bcorpus (doc, page, embedding, txt) VALUES ({doc}, {page}, '{emb}', '{txt}');"
+        query = f"INSERT INTO {table_name} (doc, page, embedding, txt) VALUES ({doc}, {page}, '{emb}', '{txt}');"
         cur.execute(query)
 
     conn.commit()
     cur.close()
     conn.close()
+    print('Setup finished')
 
 if __name__ == "__main__":
     setup_pg()
